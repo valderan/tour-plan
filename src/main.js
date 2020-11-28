14eece366ae4100b7ad484977651a19be885e2db
valderan <v.kundryukov@gmail.com>
2020-11-28 21:35:34 +0300

    fix: fix bugs

--- src/main.js
+++ src/main.js
@@ -38,13 +38,22 @@ const rewiewSlider = new Swiper('.reviews-slider', {
 
 ymaps.ready(init);
 function init(){
-    // Создание карты.
-    var myMap = new ymaps.Map("map", {
-        center: [7.83820496, 98.29882894],
-        // Уровень масштабирования. Допустимые значения:
-        // от 0 (весь мир) до 19.
-        zoom: 17
-    });
+  // Создание карты.
+  var myMap = new ymaps.Map("map", {
+      center: [7.83820496, 98.29882894],
+      // Уровень масштабирования. Допустимые значения:
+      // от 0 (весь мир) до 19.
+      zoom: 16, 
+  });
+
+  //отключаем зум колёсиком мышки
+  myMap.behaviors.disable('scrollZoom');
+    
+  //на мобильных устройствах... (проверяем по userAgent браузера)
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+      //... отключаем перетаскивание карты
+      myMap.behaviors.disable('drag');
+  } 
 }
 
 // Parallax-effect for newsletter block
